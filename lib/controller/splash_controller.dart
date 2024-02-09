082d8fdd5c1284457bf61381affa94bc082eaf84
daffa_rasyid <daffa.naufan@gmail.com>
2024-02-10 01:47:59 +0700

    fixing getstorage

--- lib/controller/splash_controller.dart
+++ lib/controller/splash_controller.dart
@@ -1,13 +1,20 @@
 import 'dart:async';
 import 'package:dating_app_clone/routes/route_name.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 class SplashController extends GetxController {
+  var storage = GetStorage();
+
   @override
   void onInit() {
     var duration = const Duration(seconds: 3);
     Timer(duration, () {
-      Get.offAllNamed(RouteName.welcomePage);
+      if (storage.read('user') != null) {
+        Get.offAllNamed(RouteName.homePage);
+      } else {
+        Get.offAllNamed(RouteName.welcomePage);
+      }
     });
     super.onInit();
   }
--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,10 @@ import 'package:dating_app_clone/routes/route_name.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 void main() async {
+  await GetStorage.init();
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
